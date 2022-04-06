529d8a7fd2c93117ec7f5482d7d0929e686b32ca
Cavan Chan <cavan.chan@gmail.com>
2022-04-06 14:19:56 -0400

    2nd attempt

---
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,75 @@
+import React, { useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./AboutMe.css";
+
+export default function AboutMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const SCREEN_CONSTSANTS = {
+    description:
+      "Cavan Mitchell Chan is a visionary with a strong drive to execute. A patient leader of men that inspires greatness around his team. His experience is vast and his desire for WINNING has gotten him to work with Fortune 500 heads and political leaders.",
+    highlights: {
+      bullets: [
+        "Front-end web and E-commerce development",
+        "Data Scraping and Data Analytics Expert",
+        "Content Strategy and Development",
+        "Growth Hacking",
+        "Entrepreneur",
+        "Sales and Marketing Trainer",
+      ],
+      heading: "Here are a Few Highlights:",
+    },
+  };
+  const renderHighlight = () => {
+    return SCREEN_CONSTSANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
+  return (
+    <div
+      className="about-me-container screen-container fade-in"
+      id={props.id || ""}
+    >
+      <div className="about-me-parent">
+        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTSANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTSANTS.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
+            </div>
+            <div className="about-me-options">
+              <button
+                className="btn primary-btn"
+                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+              >
+                {" "}
+                Hire Me{" "}
+              </button>
+              <a href="cavancv.pdf" download="Cavan Mitchell Resume.pdf">
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
